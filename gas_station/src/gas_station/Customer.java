5a0095d15df2e8c1f3f3f82faa31696ab20ae223
GunHoi <lgh509@kyonggi.ac.kr>
2023-01-27 11:29:24 +0900

    fix: printResult

--- gas_station/src/gas_station/Customer.java
+++ gas_station/src/gas_station/Customer.java
@@ -105,6 +105,6 @@ public class Customer {
 		System.out.print("  diesel: "+gasStation.getDiesel().getStock());
 		System.out.println("  lpg: "+gasStation.getLpg().getStock());
 		System.out.println("주유소 보유 금액 : "+gasStation.getMoney()+"\n");
-		System.out.println("구매자의 연료 : "+this.getFuel().getType()+"   남은 금액 : "+this.getMoney());
+		System.out.println("구매자의 연료종류: "+this.getFuel().getType()+"  구매자의 연료 잔량 : "+this.getFuel().getStock()+"  남은 금액 : "+this.getMoney());
 	}
 }
