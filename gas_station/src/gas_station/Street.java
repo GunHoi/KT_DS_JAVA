a93bca173206fb37be481601017d9ddd2dda71d8
GunHoi <lgh509@kyonggi.ac.kr>
2023-01-27 11:07:30 +0900

    feat: main

--- gas_station/src/gas_station/Street.java
+++ gas_station/src/gas_station/Street.java
@@ -5,7 +5,7 @@ public class Street {
 	/**
 	 * 가솔린 리터당 가격
 	 */
-	public static final double GASOLINE_PRICE = 1569.67;
+	public static final double GASOLIN_PRICE = 1569.67;
 	/**
 	 * 경유 리터당 가격
 	 */
@@ -16,8 +16,35 @@ public class Street {
 	public static final double LPG_PRICE = 1039.35;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		GasStation gasStation = new GasStation();
+		Fuel gasolin = new Fuel();
+		gasolin.setType("gasolin");
+		gasolin.setStock(6000);
+		
+		Fuel diesel = new Fuel();
+		diesel.setType("diesel");
+		diesel.setStock(1000);
+		
+		Fuel lpg = new Fuel();
+		lpg.setType("lpg");
+		lpg.setStock(500);
+		
+		gasStation.setGasolin(gasolin);
+		gasStation.setDiesel(diesel);
+		gasStation.setLpg(lpg);
+		gasStation.setMoney(5_000_000);
+		
+		Customer customer = new Customer();
+		
+		Fuel customerFuel = new Fuel();
+		customerFuel.setType("diesel");
+		customerFuel.setStock(50);
+		
+		customer.setFuel(customerFuel);
+		customer.setMoney(1_000_000);
+		
+		customer.buy(gasStation, "diesel", 500);
 	}
 
 }
