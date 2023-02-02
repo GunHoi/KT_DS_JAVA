498056e4a065ce8a0d72e090ddc7f62c848c0852
GunHoi <lgh509@kyonggi.ac.kr>
2023-02-02 17:50:28 +0900

    feat: Cafe Menu Management System

--- cafe/src/com/ktdsuniversity/edu/cafe/menu/mgnt/dao/MenuMgntDAOImpl.java
+++ cafe/src/com/ktdsuniversity/edu/cafe/menu/mgnt/dao/MenuMgntDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.ktdsuniversity.edu.cafe.menu.mgnt.vo.MenuMgntVO;
 
@@ -12,30 +13,59 @@ public class MenuMgntDAOImpl implements MenuMgntDAO {
 	/**
 	 * 각 종류 별 리스트
 	 * ex) 카페 - 아메리카노, 카페라떼, 에스프레소, ... etc
-	 */
-	private List <MenuMgntVO> menuList = new ArrayList<>();
-	
+	 */	
 	// 음료 종류별로 리스트를 만들어야하나?? 다른 방법은 없을까?
 	
 	/**
 	 * 각 종류가 들어갈 맵
 	 * <Integer : 1(커피) , List<MenuMgntVO> : 아메리카노, 카페라떼, 에스프레소, ...>
 	 */
-	private Map<Integer, List<MenuMgntVO>> dataSource;
+	private Map<String, List<MenuMgntVO>> dataSource;
 	
 	public MenuMgntDAOImpl() {
 		dataSource = new HashMap<>();
+		dataSource.put("커피", new ArrayList<>());
+		dataSource.put("음료", new ArrayList<>());
+		dataSource.put("티", new ArrayList<>());
+		dataSource.put("디저트", new ArrayList<>());
 	}
-
+	
 	@Override
-	public int create(int itemType, MenuMgntVO menuMgntVO) {
-		menuList.add(menuMgntVO);
-		dataSource.put(itemType, menuList);
+	public int create(String itemType, MenuMgntVO menuMgntVO) {
+		List <MenuMgntVO> itemList = dataSource.get(itemType);
+		itemList.add(menuMgntVO);
+//		dataSource.put(itemType, itemList); 써도 되고 안써도되는 코드(불필요한 코드라는 의미)
 		return 1;
 	}
 
 	@Override
-	public List<MenuMgntVO> read(int itemType) {
+	public int update(String itemType, int itemIdx, MenuMgntVO menuMgntVO) {
+		List<MenuMgntVO> itemList = dataSource.get(itemType);
+		MenuMgntVO item = itemList.get(itemIdx);
+		item.setItemName(menuMgntVO.getItemName());
+		return 1;
+	}
+	
+	@Override
+	public int delete(String itemType, int itemIdx) {
+		List<MenuMgntVO> itemList = dataSource.get(itemType);
+		itemList.remove(itemIdx);
+		return 1;
+	}
+	
+	@Override
+	public MenuMgntVO read(String itemType, int itemIdx) {
+		return dataSource.get(itemType).get(itemIdx);
+	}
+
+	@Override
+	public List<MenuMgntVO> readSome(String itemType) {
 		return dataSource.get(itemType);
 	}
+
+	@Override
+	public Map<String, List<MenuMgntVO>> readAll() {
+		return dataSource;
+	}
+
 }
